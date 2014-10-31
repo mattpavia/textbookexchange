5cb9ed16d6a8ad8faa07de900135df68f37fc5af
Matthew Pavia <matt.pavia@gmail.com>
2014-10-31 13:31:28 -0400

    Small changes

--- application/controllers/courses.php
+++ application/controllers/courses.php
@@ -4,17 +4,13 @@ class Courses extends CI_Controller {
 
 	public function index() {
 		$this->load->model('course');
-
 		$data['courses'] = $this->course->getCourses();
-
 		$this->load->view('courses', $data);
 	}
 
 	public function lookup($id) {
 		$this->load->model('course');
-		
 		$data['course'] = $this->course->getCourse($id);
-
 		$this->load->view('course', $data);
 	}
 	
--- application/controllers/textbooks.php
+++ application/controllers/textbooks.php
@@ -4,17 +4,13 @@ class Textbooks extends CI_Controller {
 
 	public function index() {
 		$this->load->model('textbook');
-
 		$data['textbooks'] = $this->textbook->getTextbooks();
-
 		$this->load->view('textbooks', $data);
 	}
 
 	public function lookup($id) {
 		$this->load->model('textbook');
-
 		$data['textbook'] = $this->textbook->getTextbook($id);
-
 		$this->load->view('textbook', $data);
 	}
 	
