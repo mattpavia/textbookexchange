3619950c2a2b26aa9311e542ba112e71188420f5
Matthew Pavia <matt.pavia@gmail.com>
2014-11-14 17:38:37 -0500

    Hide all the nav buttons if not logged in

--- application/views/header.php
+++ application/views/header.php
@@ -20,11 +20,11 @@
     <header>
         <h1>Textbook Exchange</h1>
         <ul>
+            <?php if ($this->auth_ldap->is_authenticated()) { ?>
             <li><a href="<?php echo site_url(); ?>">Home</a></li>
             <li><a href="<?php echo site_url(); ?>textbooks">Textbooks</a></li>
             <li><a href="<?php echo site_url(); ?>courses">Courses</a></li>
             <li><a href="<?php echo site_url(); ?>about">About</a></li>
-            <?php if ($this->auth_ldap->is_authenticated()) { ?>
             <li><a href="<?php echo site_url(); ?>logout">Logout</a></li>
             <?php } ?>
         </ul>
