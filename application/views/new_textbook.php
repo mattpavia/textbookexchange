ad8381f92dbaef7c6ef7e62baae8b705391cea49
Sean Fahey <smf315@lehigh.edu>
2015-03-06 13:09:54 -0500

    Update new_textbook.php

--- application/views/new_textbook.php
+++ application/views/new_textbook.php
@@ -4,6 +4,9 @@
     <div class="page_header">List a Textbook</div>
     <div class="input">
         <input type="text" name="isbn" placeholder="ISBN" />
+        <input type="text" name="author" placeholder="Author" />
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="price" placeholder="Price" />
     </div>
     <input type="submit" value="Submit" class="submit" />
 </form>
